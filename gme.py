57682a2db42242604934473fe73bed4700b7bb6c
fares-works <sxpfares@gmail.com>
2024-06-19 03:59:28 +0300

    Add files via upload

---
+++ gme.py
@@ -0,0 +1,46 @@
+import yfinance as yf
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+
+gme = yf.Ticker('GME')
+gme_data = gme.history(period = 'max')
+gme_data.reset_index(inplace=True)
+
+
+url = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/stock.html'
+src = requests.get(url).text
+soup = BeautifulSoup(src, 'lxml')
+content = soup.find_all("table", 'class'== 'historical_data_table table')
+gme_revenue = pd.DataFrame(columns=['Date','Revenue'])
+for row in content[1].find_all('tr'):
+  info = row.find_all('td')
+  if(info != []):
+    date = info[0].text
+    revenue = info[1].text
+    gme_revenue = gme_revenue._append({'Date':date, 'Revenue':revenue},ignore_index=True)
+gme_revenue["Revenue"] = gme_revenue['Revenue'].str.replace(',|\$',"")
+gme_revenue.dropna(inplace=True)
+gme_revenue = gme_revenue[gme_revenue['Revenue'] != ""]
+
+def make_graph(stock_data, revenue_data, stock):
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, subplot_titles=("Historical Share Price", "Historical Revenue"), vertical_spacing = .3)
+    stock_data_specific = stock_data[stock_data.Date <= '2021--06-14']
+    revenue_data_specific = revenue_data[revenue_data.Date <= '2021-04-30']
+    fig.add_trace(go.Scatter(x=pd.to_datetime(stock_data_specific.Date, infer_datetime_format=True), y=stock_data_specific.Close.astype("float"), name="Share Price"), row=1, col=1)
+    fig.add_trace(go.Scatter(x=pd.to_datetime(revenue_data_specific.Date, infer_datetime_format=True), y=revenue_data_specific.Revenue, name="Revenue"), row=2, col=1)
+    fig.update_xaxes(title_text="Date", row=1, col=1)
+    fig.update_xaxes(title_text="Date", row=2, col=1)
+    fig.update_yaxes(title_text="Price ($US)", row=1, col=1)
+    fig.update_yaxes(title_text="Revenue ($US Millions)", row=2, col=1)
+    fig.update_layout(showlegend=False,
+    height=900,
+    title=stock,
+    xaxis_rangeslider_visible=True)
+    fig.show()
+
+
+make_graph(gme_data, gme_revenue, 'GameStop')
